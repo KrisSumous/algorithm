165a5928845028f4a08daf08c687afcb367911e0
nihuan <nihuan@51yund.com>
2021-07-16 17:23:33 +0800

    initiate

---
+++ src/com/kris/queue/Queue.java
@@ -0,0 +1,26 @@
+package com.kris.queue;
+
+public class Queue<E> {
+
+    public void enqueue(E e) {
+
+    }
+
+    public E dequeue() {
+        return null;
+    }
+
+    public E getFront() {
+        return null;
+    }
+
+    public int getSize() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+
+}
